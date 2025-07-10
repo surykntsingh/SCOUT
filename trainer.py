baec3224ce279a3e01a4891c849de1d7c5de71f7
Suryakant Singh <surykntsingh@gmail.com>
2025-07-10 08:23:15 -0400

    debug

--- trainer.py
+++ trainer.py
@@ -31,7 +31,7 @@ class Trainer:
             callbacks=[checkpoint_callback, early_stop_callback],
             accelerator='gpu',
             devices=[1,2,3],
-            strategy='auto',
+            strategy='ddp_find_unused_parameters_true',
             enable_progress_bar=True,
             log_every_n_steps=2,
             fast_dev_run=fast_dev_run
@@ -48,7 +48,7 @@ class Trainer:
             trainer = pl.Trainer(
                 accelerator='gpu',
                 devices=[1,2,3],
-                strategy='auto',
+                strategy='ddp_find_unused_parameters_true',
                 enable_progress_bar=True,
                 log_every_n_steps=2,
                 fast_dev_run=fast_dev_run
