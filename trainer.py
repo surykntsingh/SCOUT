b7ba1a5402ee4a36db3bcb8c708deec13defd2b6
Suryakant Singh <surykntsingh@gmail.com>
2025-07-12 19:57:00 -0400

    added typer app

--- trainer.py
+++ trainer.py
@@ -27,7 +27,7 @@ class Trainer:
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
         early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=5, verbose=True, mode="min")
-        self.trainer = pl.Trainer(
+        trainer = pl.Trainer(
             max_epochs=self.max_epochs,
             callbacks=[checkpoint_callback, early_stop_callback],
             accelerator='gpu',
@@ -37,32 +37,25 @@ class Trainer:
             log_every_n_steps=2,
             fast_dev_run=fast_dev_run
         )
-        self.trainer.fit(
+        trainer.fit(
             self.model, datamodule=self.datamodule
         )
-        train_metrics = self.trainer.logged_metrics
+        train_metrics = trainer.logged_metrics
         return train_metrics
 
-    def test(self, model=None, fast_dev_run=False):
+    def test(self, fast_dev_run=False):
 
-        if model is not None:
-            print('---------------------> if')
-            trainer = pl.Trainer(
-                accelerator='gpu',
-                devices=self.__devices,
-                strategy='ddp_find_unused_parameters_true',
-                enable_progress_bar=True,
-                log_every_n_steps=2,
-                fast_dev_run=fast_dev_run
-            )
-        else:
-            print('---------------------> else')
-            model = self.model
-            trainer = self.trainer
+        trainer = pl.Trainer(
+            accelerator='gpu',
+            devices=self.__devices,
+            strategy='ddp_find_unused_parameters_true',
+            enable_progress_bar=True,
+            log_every_n_steps=2,
+            fast_dev_run=fast_dev_run
+        )
 
-        print(model, trainer)
         trainer.test(
-            model, datamodule=self.datamodule
+            self.model, datamodule=self.datamodule
         )
         test_metrics = trainer.logged_metrics
         return test_metrics
