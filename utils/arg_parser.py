c27dc8e171750c24bf41dc76be6bfaaa63c83915
Suryakant Singh <surykntsingh@gmail.com>
2025-07-14 10:24:50 -0400

    Updated model

--- utils/arg_parser.py
+++ utils/arg_parser.py
@@ -48,6 +48,6 @@ def parse_agrs():
     parser.add_argument('--model_save_path', type=str, default='/mnt/surya/projects/Wsi-rgen/saved_models/mi_gen_titan_plus_conch1.5_20250712_1_model.ckpt',
                         help='model save path')
     parser.add_argument('--devices', type=str, default='1,2,3,4,5', help='devices.')
-    parser.add_argument('--fastdevrun', type=str, default='fastdevrun', help='fast dev run.')
+    parser.add_argument('--fastdevrun', type=bool, default='false', help='fast dev run.')
     args = parser.parse_args()
     return args
