8ca4be6cd63380084bbf72e5e36da0832a1343b7
Suryakant Singh <surykntsingh@gmail.com>
2025-08-09 09:46:00 -0400

    changed scheduler

--- main.py
+++ main.py
@@ -53,12 +53,10 @@ def trainkfold(config_file_path='config.yaml'):
 
 @app.command()
 def test(config_file_path='config.yaml'):
-    model_path = 'saved_models/mi_gen_titan_plus_conch1.5_20250711_1_model.ckpt'
+
     args = get_params_for_key(config_file_path, "train")
     split_frac = [0.8, 0.12, 0.08]
     tokenizer = Tokenizer(args.reports_json_path)
-    model = ReportModel.load_from_checkpoint(model_path, args=args, tokenizer=tokenizer)
-
     datamodule = PatchEmbeddingDataModule(args, tokenizer, split_frac)
     trainer = Trainer(args, tokenizer, split_frac)
 
