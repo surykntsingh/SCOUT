ce3641b49ce5001531847928bd68083923f4c828
Suryakant Singh <surykntsingh@gmail.com>
2025-08-10 21:01:31 -0400

    added layernorm

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = typer.Typer()
 @app.command()
 def train(config_file_path='config.yaml'):
     args = get_params_for_key(config_file_path, "train")
-    split_frac = [0.75, 0.2, 0.05]
+    split_frac = [0.8, 0.12, 0.08]
     tokenizer = Tokenizer(args.reports_json_path)
     model = ReportModel(args, tokenizer)
 
