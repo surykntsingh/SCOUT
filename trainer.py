bd010f7920fd93c8e2ef039ddac07a1ff1db971f
Suryakant Singh <surykntsingh@gmail.com>
2025-08-11 23:06:07 -0400

    updated trainer

--- trainer.py
+++ trainer.py
@@ -46,15 +46,15 @@ class Trainer:
             save_top_k=1,  # Number of best checkpoints to keep
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
-        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=7, verbose=True, mode="min")
+        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=9, verbose=True, mode="min")
         self.trainer = pl.Trainer(
             max_epochs=self.max_epochs,
             callbacks=[checkpoint_callback, early_stop_callback],
             accelerator='gpu',
             devices=self.devices,
-            strategy='ddp_find_unused_parameters_true',
+            strategy='ddp',
             enable_progress_bar=True,
-            log_every_n_steps=2,
+            log_every_n_steps=1,
             fast_dev_run=fast_dev_run
         )
         self.trainer.fit(
@@ -174,9 +174,9 @@ class KFoldTrainer(Trainer):
                 callbacks=[checkpoint_callback, early_stop_callback],
                 accelerator='gpu',
                 devices=self.devices,
-                strategy='ddp_find_unused_parameters_true',
+                strategy='ddp',
                 enable_progress_bar=True,
-                log_every_n_steps=2,
+                log_every_n_steps=1,
                 fast_dev_run=fast_dev_run
             )
 
