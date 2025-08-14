f3b0347c9c8da9a6a3b4000ef7fc989fa3afc6fa
Suryakant Singh <surykntsingh@gmail.com>
2025-08-14 18:30:27 -0400

    changed scheduler

--- trainer.py
+++ trainer.py
@@ -44,7 +44,7 @@ class Trainer:
             save_top_k=1,  # Number of best checkpoints to keep
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
-        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=7, verbose=True, mode="min")
+        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=21, verbose=True, mode="min")
         self.trainer = pl.Trainer(
             max_epochs=self.max_epochs,
             callbacks=[checkpoint_callback, early_stop_callback],
@@ -164,7 +164,7 @@ class KFoldTrainer(Trainer):
                 save_top_k=1,  # Number of best checkpoints to keep
                 save_last=True  # Save the last checkpoint regardless of the monitored metric
             )
-            early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=23, verbose=True,
+            early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=7, verbose=True,
                                                 mode="min")
             trainer = pl.Trainer(
                 max_epochs=self.max_epochs,
