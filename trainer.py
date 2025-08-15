7cc4ab48b9ad07914d3adac37fa811d141e38808
Suryakant Singh <surykntsingh@gmail.com>
2025-08-15 12:23:01 -0400

    added predict

--- trainer.py
+++ trainer.py
@@ -24,8 +24,8 @@ class Trainer:
         self.datamodule = PatchEmbeddingDataModule(args, tokenizer, split_frac)
         # self.model = ReportModel(args, tokenizer)
         pl.seed_everything(42)
-        torch.set_float32_matmul_precision('high')
-        torch.use_deterministic_algorithms(True)
+        # torch.set_float32_matmul_precision('high')
+        # torch.use_deterministic_algorithms(True)
         self.trainer = None
         self.devices = list(map(int, args.devices.split(',')))
         self.args = args
