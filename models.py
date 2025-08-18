14793142e6bd39e17fa36a88fa3537b7c1dac246
Suryakant Singh <surykntsingh@gmail.com>
2025-08-17 21:51:47 -0400

    saved results

--- models.py
+++ models.py
@@ -65,9 +65,9 @@ class ReportModel(pl.LightningModule):
         loss = self.loss_fn(output_, report_ids, report_masks)
         self.log('val_loss', loss, on_epoch=True, prog_bar=True, sync_dist=True)
 
-        # if batch_idx % 10==0:
-        output = self.model(feats1, gecko_feats, gecko_concepts, report_ids, patch_masks, mode='sample')
-        pred_texts = self.tokenizer.batch_decode(output.cpu().numpy())
+        if batch_idx % 10==0:
+            output = self.model(feats1, gecko_feats, gecko_concepts, report_ids, patch_masks, mode='sample')
+            pred_texts = self.tokenizer.batch_decode(output.cpu().numpy())
         # target_texts = self.tokenizer.batch_decode(report_ids[:, 1:].cpu().numpy())
 
         target_texts = [self.reports[slide_id] for slide_id in slide_ids]
