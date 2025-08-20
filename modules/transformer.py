c82ae9d2b8767c24def98dc2735d2ad27a11015e
Suryakant Singh <surykntsingh@gmail.com>
2025-08-20 19:03:53 -0400

    new transformer

--- modules/transformer.py
+++ modules/transformer.py
@@ -155,7 +155,7 @@ class EncoderDecoder(AttModel):
                 self.num_layers),
             LayerNorm(self.d_model),
             nn.Sequential(Embeddings(self.d_model, tgt_vocab), deepcopy(position)),
-            nn.Sequential(Embeddings(tgt_vocab, tgt_vocab), deepcopy(position))
+            nn.Sequential(Embeddings(self.d_model, tgt_vocab), deepcopy(position))
         )
         return model
 
