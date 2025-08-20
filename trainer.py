29bfa749ae691e50a963680a841bdc29256f4a03
Suryakant Singh <surykntsingh@gmail.com>
2025-08-20 13:04:42 -0400

    added tune step

--- trainer.py
+++ trainer.py
@@ -70,7 +70,7 @@ class Trainer:
             log_every_n_steps=1,
             fast_dev_run=fast_dev_run
         )
-        
+
         self.trainer.fit(
             model, datamodule=datamodule
         )
@@ -85,6 +85,7 @@ class Trainer:
         trainer = pl.Trainer(
             accelerator='gpu',
             devices=self.devices,
+            strategy='ddp_find_unused_parameters_true',
             enable_progress_bar=True
         )
         tuner = Tuner(trainer)
