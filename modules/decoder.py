ae09c7b1a54b73e27e9fa35a8ee6f17d4b0d74d2
Suryakant Singh <surykntsingh@gmail.com>
2025-08-20 20:45:13 -0400

    new transformer

--- modules/decoder.py
+++ modules/decoder.py
@@ -26,8 +26,8 @@ class DecoderLayer(nn.Module):
 
         x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
         print(f'x: {x.shape}')
-        # x = self.sublayer[2](x, lambda x: self.concept_attn(x, concepts, concepts))
-        return self.sublayer[2](x, self.feed_forward)
+        x = self.sublayer[2](x, lambda x: self.concept_attn(x, concepts, concepts))
+        return self.sublayer[3](x, self.feed_forward)
 
 
 class Decoder(nn.Module):
