5362d64647f4c8af54b5380a00d40333db99dc3f
Suryakant Singh <surykntsingh@gmail.com>
2025-09-30 15:17:25 -0400

    Updated decoder

--- modules/decoder.py
+++ modules/decoder.py
@@ -26,7 +26,7 @@ class DecoderLayer(nn.Module):
 
         x1 = self.sublayer[1](x0, lambda x: self.src_attn(x, hidden_states, hidden_states, src_mask))
         print(f'x1: {x1.shape}, concepts: {concepts.shape}')
-        x2 = self.sublayer[2](feats, lambda x: self.concept_attn(x, concepts, concepts))
+        x2 = self.sublayer[2](feats, lambda x: self.concept_attn(x, concepts, x))
         print(f'---->>x2: {x2.shape}, concepts: {concepts.shape}')
         # x =
 
