d8d28370010b8f1e8e1f504817dfc11f45be70fd
Suryakant Singh <surykntsingh@gmail.com>
2025-10-06 15:16:53 -0400

    added script

--- copy_images.py
+++ copy_images.py
@@ -10,22 +10,20 @@ def read_json_file(json_path):
         d = json.load(f)
     return d
 
-def get_file_paths(patient_id, tcga_dir):
-    slide_paths={}
-    for root, _, files in os.walk(tcga_dir):
-        for file in files:
-            if file.endswith(".svs") and patient_id in file:
-                full_path = os.path.join(root, file)
-                slide_type = file.split('.')[0].split('-')[-1][:2]
-                try:
-                    slide = openslide.OpenSlide(full_path)
-                    magnification = slide.properties.get(openslide.PROPERTY_NAME_OBJECTIVE_POWER, "Unknown")
-                    slide_paths[slide_type] = {'file_name': file, 'file_path': full_path, 'mag': magnification}
-                    slide.close()
-                except Exception as e:
-                    print(f'full_path: {full_path} e: {e}')
-                    slide_paths[slide_type] = {'file_name': file, 'file_path': full_path, 'mag': 'Unknown'}
-    return slide_paths
+def copy_images(args):
+
+    pathtext = read_json_file(args.pathtext_json_path)
+    pbar = tqdm(pathtext, total=len(pathtext))
+    mags = set()
+    for i, p in enumerate(pbar):
+        mag = p['paths']['DX']['mag']
+        if mag not in mags:
+            os.makedirs(f'{args.target_dir}/{mag}', exist_ok=True)
+        destination_path = os.path.join(args.target_dir, f'{mag}/{p["id"]}.svs')
+        shutil.copy(p['paths']['DX']['file_path'], destination_path)
+    #
+
+    print(mags)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -35,16 +33,5 @@ if __name__ == "__main__":
     parser.add_argument('--target_dir', type=str,
                         help='target_dir')
     args = parser.parse_args()
-    reports_data = []
-
-    pathtext = read_json_file(args.pathtext_json_path)
-    pbar = tqdm(pathtext, total=len(pathtext))
-    mags = set()
-    for i, p in enumerate(pbar):
-        mag = p['paths']['DX']['mag']
-        mags.add(mag)
-        # os.makedirs(f'{args.ckpt_path}/results', exist_ok=True)
-
-
-    print(mags)
+    copy_images(args)
     print('Finished!')
