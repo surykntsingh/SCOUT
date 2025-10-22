69a6b69aee5514588992b35125350971841ddadf
Suryakant Singh <surykntsingh@gmail.com>
2025-10-21 22:59:13 -0400

    updated scripts for tcga

--- embedding_datasets/embedding_dataset.py
+++ embedding_datasets/embedding_dataset.py
@@ -18,10 +18,11 @@ class EmbeddingDataset(Dataset):
         self.__gecko_emb_path = gecko_emb_path
 
         files = os.listdir(embeddings_path)
-        files_1 = os.listdir(embeddings_path)
-        self.__slides = [file.split('.')[0] for file in files if file in files_1]
+        files_1 = os.listdir(embeddings_path_2)
+        files_2 = os.listdir(gecko_emb_path)
+        self.__slides = [file.split('.')[0] for file in files if file in files_1 and file in files_2]
 
-        print(f'files: {len(files)}, files_1: {len(files_1)}')
+        print(f'files: {len(files)}, files_1: {len(files_1)}, files_2: {len(files_2)} slides: {len(self.__slides)}')
 
     def __len__(self):
         return len(self.__slides)
