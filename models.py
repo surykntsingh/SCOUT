44665b3a0f1734db6e7d08ce2d0c733feb239cd1
Suryakant Singh <surykntsingh@gmail.com>
2025-10-23 13:41:11 -0400

    updated trainer

--- models.py
+++ models.py
@@ -58,7 +58,7 @@ class ReportModel(pl.LightningModule):
         # print(f'train output: {output}')
         loss = self.loss_fn(output, report_ids, report_masks)
         self.log('train_loss', loss, on_epoch=True, prog_bar=True, sync_dist=True)
-        if batch_idx %2==0:
+        if batch_idx %100==0:
             print(
                 f"[GPU] Alloc: {torch.cuda.memory_allocated() / 1e6:.1f} MB | Reserved: {torch.cuda.memory_reserved() / 1e6:.1f} MB")
 
