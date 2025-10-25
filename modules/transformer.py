c2e7326d55e89cdbafbfbbb7e62ee06b93ef3404
Suryakant Singh <surykntsingh@gmail.com>
2025-10-25 18:00:34 -0400

    updated trainer

--- modules/transformer.py
+++ modules/transformer.py
@@ -52,7 +52,7 @@ class MultiHeadedAttention(nn.Module):
 
         x, self.attn = self.attention(query, key, value, mask=mask, dropout=self.dropout)
 
-        x = x.transpose(1, 2).view(nbatches, -1, self.h * self.d_k).contiguous()
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k).contiguous()
         return self.linears[-1](x)
 
     def attention(self, query, key, value, mask=None, dropout=None):
