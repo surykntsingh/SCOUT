80abd5d65e1926e86d646e6575df696bdb6d710a
Suryakant Singh <surykntsingh@gmail.com>
2025-10-30 00:55:28 -0400

    added cross attention

--- modules/attention_model.py
+++ modules/attention_model.py
@@ -36,7 +36,7 @@ class AttModel(CaptionModel):
                 ((nn.BatchNorm1d(self.input_encoding_size),) if self.use_bn == 2 else ())))
 
         self.gc_embed = nn.Sequential(
-            nn.Linear(1, 1),
+            nn.Linear(254, 1),
             nn.ReLU(),
             nn.Dropout(self.drop_prob_lm)
         )
--- modules/transformer.py
+++ modules/transformer.py
@@ -187,7 +187,7 @@ class EncoderDecoder(AttModel):
         att_feats, gc_feats, seq, _, att_masks, seq_mask, _ = self._prepare_feature_forward(
             att_feats, gc_feats, att_masks, meshes
         )
-        #missing gc_feats
+
         memory = self.model.encode(att_feats, att_masks)
 
         return fc_feats[..., :1], att_feats[..., :1], memory, gc_feats, att_masks
