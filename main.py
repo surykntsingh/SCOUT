029e3a1559c9f82572f6a69dc2d1ee7e9f6a7df8
Suryakant Singh <surykntsingh@gmail.com>
2025-10-31 00:33:13 -0400

    added cross attention

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def tune_gecko_features(args, tokenizer,best_model_path, trainer, datamodule, re
 def test(config_file_path: str='histai_config.yaml', reg_threshold: float=0.8):
 
     args = get_params_for_key(config_file_path, "train")
-    split_frac = [0.85, 0.14, 0.01]
+    split_frac = [0.85, 0.1, 0.05]
     tokenizer = Tokenizer(args.reports_json_path)
     datamodule = PatchEmbeddingDataModule(args, tokenizer, split_frac)
     trainer = Trainer(args, tokenizer, split_frac)
