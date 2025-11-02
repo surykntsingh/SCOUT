d45a3f930a59ebd801039ff1c0adf11f8a9f49f8
Suryakant Singh <surykntsingh@gmail.com>
2025-11-01 20:14:06 -0400

    added cross attention

--- models.py
+++ models.py
@@ -107,8 +107,8 @@ class ReportModel(pl.LightningModule):
 
                 target_texts = [self.reports[slide_id] for slide_id in slide_ids]
 
-                gts = [{slide_id: self.reports[slide_id]} for slide_id in slide_ids]
-                preds = [{slide_id: pred_texts[i]} for i,slide_id in enumerate(slide_ids)]
+                gts = {slide_id: self.reports[slide_id] for slide_id in slide_ids}
+                preds = {slide_id: pred_texts[i] for i,slide_id in enumerate(slide_ids)}
 
                 rouge_score = float(self.val_rouge(pred_texts, target_texts)['rouge1_fmeasure'].to('cpu'))
                 bleu_score1 = self.val_bleu(pred_texts, target_texts).to(self.device)
@@ -148,8 +148,8 @@ class ReportModel(pl.LightningModule):
 
             target_texts = [self.reports[slide_id] for slide_id in slide_ids]
 
-            gts = [{slide_id: self.reports[slide_id]} for slide_id in slide_ids]
-            preds = [{slide_id: pred_texts[i]} for i, slide_id in enumerate(slide_ids)]
+            gts = {slide_id: self.reports[slide_id] for slide_id in slide_ids}
+            preds = {slide_id: pred_texts[i] for i, slide_id in enumerate(slide_ids)}
 
             if batch_idx % 100 == 0:
                 RED = '\033[91m'
