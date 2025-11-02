f0cfd14f40e03ef3572f03e6e906b37f8a35c762
Suryakant Singh <surykntsingh@gmail.com>
2025-11-02 01:36:15 -0400

    added cross attention

--- modules/decoder.py
+++ modules/decoder.py
@@ -27,7 +27,7 @@ class DecoderLayer(nn.Module):
         # print(f'x1: {x1.shape}, concepts: {concepts.shape}')
         x2 = self.sublayer[2](x0, lambda x: self.concept_attn(x, concepts, concepts))
         # print(f'---->>x2: {x2.shape}, concepts: {concepts.shape}')
-        x = self.sublayer[3](x1, self.feed_forward) + self.sublayer[4](x2, self.feed_forward)
+        x = self.sublayer[3](x1, self.feed_forward) #+ self.sublayer[4](x2, self.feed_forward)
         # print(f'---->>x: {x.shape}, concepts: {concepts.shape}')
         return x
 
