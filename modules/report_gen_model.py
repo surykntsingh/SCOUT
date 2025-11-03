3179d6c4401ef3e9452456e8e7243a8db8d06f49
Suryakant Singh <surykntsingh@gmail.com>
2025-11-03 12:03:40 -0500

    Updated concept embeddings

--- modules/report_gen_model.py
+++ modules/report_gen_model.py
@@ -108,7 +108,7 @@ class ReportGenModel(nn.Module):
         image_embeddings1 = self.adapter_mlp_1(image_embeddings1)
         image_embeddings2 = self.adapter_mlp_2(image_embeddings2)
 
-        emb_gc_proj = self.gecko_mlp(emb_gc)
+        emb_gc_proj = self.gecko_mlp(emb_gc.unsqueeze(1))
         #
         gecko_embeddings = self.gecko_encoder(torch.cat([emb_g,emb_gc_proj], dim=1))
 
