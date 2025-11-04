4fff1ac4bb53b1ba44749339738a447f65969005
Suryakant Singh <surykntsingh@gmail.com>
2025-11-03 23:24:47 -0500

    Updated concept embeddings

--- modules/caption_model.py
+++ modules/caption_model.py
@@ -197,7 +197,7 @@ class CaptionModel(nn.Module):
                     # move the current group one step forward in time
 
                     it = beam_seq_table[divm][:, :, t - divm].reshape(-1)
-                    logprobs_table[divm], state_table[divm] = self.get_logprobs_state(it, *(
+                    logprobs_table[divm], state_table[divm],_ = self.get_logprobs_state(it, *(
                             args[divm] + [state_table[divm]]), gc_feats=gc_feats)
                     logprobs_table[divm] = F.log_softmax(logprobs_table[divm] / temperature, dim=-1)
 
