f9890197dc8eec78c8a8c3e5443eebd0e315b363
Suryakant Singh <surykntsingh@gmail.com>
2025-11-09 15:45:53 -0500

    Added cocept activations

--- modules/report_gen_model.py
+++ modules/report_gen_model.py
@@ -125,7 +125,7 @@ class ReportGenModel(nn.Module):
         patch_feats = self.encoder(patch_feats)
         att_feats = torch.cat([self.prompt, patch_feats], dim=1)
         fc_feats = torch.sum(att_feats, dim=1)
-        attn_gc = self.concept_encoder(attn_gc)
+        attn_gc = self.concept_encoder(emb_gc)
 
         if mode == 'train':
             output, concept_attn_maps, concept_tokens = self.encoder_decoder(fc_feats, att_feats, attn_gc, report_ids, mode='forward')
