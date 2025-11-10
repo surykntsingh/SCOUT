d42be6505900383538e70f4e8ef2860ee2605833
Suryakant Singh <surykntsingh@gmail.com>
2025-11-09 20:10:11 -0500

    added pycocevalcap

--- modules/pycocoevalcap/meteor/meteor.py
+++ modules/pycocoevalcap/meteor/meteor.py
@@ -32,35 +32,45 @@ class Meteor:
         self.lock = threading.Lock()
 
     def compute_score(self, gts, res):
-        assert(gts.keys() == res.keys())
+        assert (gts.keys() == res.keys())
         imgIds = gts.keys()
         scores = []
 
         eval_line = 'EVAL'
         self.lock.acquire()
         for i in imgIds:
-            assert(len(res[i]) == 1)
+            assert (len(res[i]) == 1)
             stat = self._stat(res[i][0], gts[i])
             eval_line += ' ||| {}'.format(stat)
 
-        # self.meteor_p.stdin.write('{}\n'.format(eval_line))
-        self.meteor_p.stdin.write('{}\n'.format(eval_line).replace('.', ','))
-        for i in range(0,len(imgIds)):
+        self.meteor_p.stdin.write('{}\n'.format(eval_line).encode())
+        self.meteor_p.stdin.flush()
+        for i in range(0, len(imgIds)):
             scores.append(float(self.meteor_p.stdout.readline().strip()))
         score = float(self.meteor_p.stdout.readline().strip())
         self.lock.release()
 
         return score, scores
 
+    def _stat(self, hypothesis_str, reference_list):
+        # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
+        hypothesis_str = hypothesis_str.replace('|||', '').replace('  ', ' ')
+        score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
+        self.meteor_p.stdin.write('{}\n'.format(score_line).encode())
+        self.meteor_p.stdin.flush()
+        raw = self.meteor_p.stdout.readline().decode().strip()
+        numbers = [str(int(float(n))) for n in raw.split()]
+        return ' '.join(numbers)
+
     def method(self):
         return "METEOR"
 
-    def _stat(self, hypothesis_str, reference_list):
-        # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
-        hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
-        score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        self.meteor_p.stdin.write('{}\n'.format(score_line))
-        return self.meteor_p.stdout.readline().strip()
+    # def _stat(self, hypothesis_str, reference_list):
+    #     # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
+    #     hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
+    #     score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
+    #     self.meteor_p.stdin.write('{}\n'.format(score_line))
+    #     return self.meteor_p.stdout.readline().strip()
 
     def _score(self, hypothesis_str, reference_list):
         self.lock.acquire()
