ec696812ac5399e5b245fee92299ca6285283718
Suryakant Singh <surykntsingh@gmail.com>
2025-11-09 20:41:17 -0500

    added pycocevalcap

--- modules/pycocoevalcap/meteor/meteor.py
+++ modules/pycocoevalcap/meteor/meteor.py
@@ -57,6 +57,7 @@ class Meteor:
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
         hypothesis_str = hypothesis_str.replace('|||', '').replace('  ', ' ')
         score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
+        score_line = score_line.replace('\n', '').replace('\r', '')
         self.meteor_p.stdin.write('{}\n'.format(score_line).replace('.', ',').encode())
         self.meteor_p.stdin.flush()
         raw = self.meteor_p.stdout.readline().decode().strip()
