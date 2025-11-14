312539f5da2368bda0beb38f31e5ed3eddaa4d88
Suryakant Singh <surykntsingh@gmail.com>
2025-11-14 10:23:42 -0500

    cleanup

--- modules/report_gen_model.py
+++ modules/report_gen_model.py
@@ -179,7 +179,7 @@ class ReportGenModel(nn.Module):
         # gecko_embeddings = self.gecko_encoder(emb_g)
 
         # patch_feats = torch.cat([image_embeddings1, image_embeddings2, gecko_embeddings], dim=1)
-        patch_feats = self.encoder(image_embeddings1)
+        patch_feats = self.encoder(image_embeddings2)
         att_feats = torch.cat([self.prompt, patch_feats], dim=1)
         fc_feats = torch.sum(att_feats, dim=1)
         attn_gc = self.concept_encoder(attn_gc)
