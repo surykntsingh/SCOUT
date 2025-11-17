96e107bb5be75da78af3a7c7f20dad92a1bb7dc5
Suryakant Singh <surykntsingh@gmail.com>
2025-11-17 05:35:14 -0500

    updated transformer

--- modules/encoder.py
+++ modules/encoder.py
@@ -22,7 +22,7 @@ class Encoder(nn.Module):
         for i,layer in enumerate(self.layers):
             # x = self.concept_fusion(self.norm(x), concepts)
             x = layer(self.norm(x), mask)
-            x = self.PAM[i](x)
+            # x = self.PAM[i](x)
 
             s.append(x)
 
