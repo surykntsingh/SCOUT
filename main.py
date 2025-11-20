0597026a8a582796b3d2f3bb15d2644657070bb4
Suryakant Singh <surykntsingh@gmail.com>
2025-11-20 00:39:45 -0500

    new tokenizer

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ torch.autograd.set_detect_anomaly(True)
 @app.command()
 def train(config_file_path: str='histai_config.yaml', notes: str=''):
     args = get_params_for_key(config_file_path, "train")
-    split_frac = [0.80, 0.1, 0.1]
+    split_frac = [0.82, 0.09, 0.09]
     tokenizer = Tokenizer(args.reports_json_path)
     if args.resume:
         print(f'Resuming training... Loading model from {args.model_load_path}')
