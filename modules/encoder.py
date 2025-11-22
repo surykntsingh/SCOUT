854037bac826445a266ff512c2b7557181055538
Suryakant Singh <surykntsingh@gmail.com>
2025-11-21 20:57:07 -0500

    updated supervision head

--- modules/encoder.py
+++ modules/encoder.py
@@ -12,7 +12,7 @@ class Encoder(nn.Module):
         super().__init__()
         self.layers = clones(layer, N)
         self.norm = LayerNorm(layer.d_model)
-        self.PAM = clones(PAM, N)
+        # self.PAM = clones(PAM, N)
         self.N = N
         self.concept_sublayer = clones(ConceptSublayer(layer.d_model, concept_fusion), N)
         self.layer_weights = nn.Parameter(torch.ones(N))
