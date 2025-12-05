705743e7c6e93e31c3e8c45b901a0acf6dfc7f03
Suryakant Singh <surykntsingh@gmail.com>
2025-12-04 20:39:09 -0500

    no gc embed

--- modules/report_gen_model.py
+++ modules/report_gen_model.py
@@ -231,7 +231,7 @@ class ReportGenModel(nn.Module):
         att_feats = torch.cat([self.prompt, patch_feats], dim=1)
         # att_feats = self.prompt
         fc_feats = torch.sum(att_feats, dim=1)
-        # attn_gc = self.concept_encoder(attn_gc)
+        attn_gc = self.concept_encoder(attn_gc)
 
         if mode == 'train':
             output, concept_attn_maps, concept_tokens = self.encoder_decoder(fc_feats, att_feats, attn_gc, report_ids, mode='forward')
