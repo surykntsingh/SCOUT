b0579dc2de564c6913469d306a1e2c230bd9db5a
Suryakant Singh <surykntsingh@gmail.com>
2025-12-07 18:28:44 -0500

    added train metrics

--- models.py
+++ models.py
@@ -227,6 +227,7 @@ class ReportModel(pl.LightningModule):
 
     def __save_predictions(self, slide_ids, pred_texts, ground_truths):
         # print(f'slide_ids: {slide_ids}, pred_texts: {pred_texts}')
+        pred_texts = list(map(lambda x: 'placeholder' if x.strip() == '' else x, pred_texts))
         for i, slide_id in enumerate(slide_ids):
             self.predictions[slide_id] = {
                 'pred': pred_texts[i],
