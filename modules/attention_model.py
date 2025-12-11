09efd91c2405cb9e67d2be31b448bfabe0d248ec
Suryakant Singh <surykntsingh@gmail.com>
2025-12-11 08:24:23 +0530

    arch_v2

--- modules/attention_model.py
+++ modules/attention_model.py
@@ -100,7 +100,7 @@ class AttModel(CaptionModel):
         # first step, feed bos
         it = fc_feats.new_full([batch_size], self.bos_idx, dtype=torch.long)
 
-        logprobs, state, concept_attn_maps = self.get_logprobs_state(it, p_fc_feats, p_att_feats, encoded_features, p_att_masks, state, gc_feats=gc_feats)
+        logprobs, state, concept_attn_maps = self.get_logprobs_state(it, p_fc_feats, p_att_feats, encoded_features, p_att_masks, state)
 
         p_fc_feats, p_att_feats, pp_att_feats, p_att_masks, gc_feats = utils.repeat_tensors(beam_size,
                                                                                   [p_fc_feats, p_att_feats,
