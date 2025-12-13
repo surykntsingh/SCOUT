4b88d01da4ee106e5f403b326dd462fe1cda1d8d
Suryakant Singh <surykntsingh@gmail.com>
2025-12-13 23:33:53 +0530

    arch_v2

--- report_tokenizers.py
+++ report_tokenizers.py
@@ -152,7 +152,7 @@ class Tokenizer:
         # 4. Clean each sentence: remove punctuation, quotes, noise
         # -------------------------------------------------------
         def clean_sentence(s):
-            s = re.sub(r'[#,?;*!^&_+():\-\[\]{}]', '', s)
+            s = re.sub(r'[#,?;*!^&_+:\-\[\]{}]', '', s)
             s = s.replace('"', '').replace("'", "").replace("\\", "")
             return s.strip().lower()
 
