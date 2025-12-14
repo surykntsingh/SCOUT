a9c9b2ab6880c2fe6bea01ecf826a0a4074cd9e1
Suryakant Singh <surykntsingh@gmail.com>
2025-12-14 17:46:18 +0530

    arch_v2

--- trainer.py
+++ trainer.py
@@ -40,18 +40,18 @@ class Trainer:
 
         checkpoint_callback = ModelCheckpoint(
             dirpath=self.args.ckpt_path,  # Directory to save checkpoints
-            filename="model_{epoch:02d}_{val_loss:.5f}_{val_reg:.5f}",  # Naming convention
+            filename="model_{epoch:02d}_{val_loss:.5f}_{val_rouge:.5f}",  # Naming convention
             monitor="val_loss",  # Metric to monitor for saving best checkpoints
             mode="min",  # Whether to minimize or maximize the monitored metric
             save_top_k=1,  # Number of best checkpoints to keep
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
-        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=5, verbose=True, mode="min")
+        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=7, verbose=True, mode="min")
 
-        # suggested_lr = self.find_lr(model, datamodule)
-        # print(f'setting lr: {suggested_lr}')
-        # # Set suggested LR
-        # model.hparams.lr = suggested_lr
+        suggested_lr = self.find_lr(model, datamodule)
+        print(f'setting lr: {suggested_lr}')
+        # Set suggested LR
+        model.hparams.lr = suggested_lr
 
         self.trainer = pl.Trainer(
             # precision="bf16-mixed",
