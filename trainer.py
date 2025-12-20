dd1373555fe2f4ca47e7e0a03c04075683f31b9d
Suryakant Singh <surykntsingh@gmail.com>
2025-12-20 08:49:14 +0530

    arch_v2

--- trainer.py
+++ trainer.py
@@ -41,8 +41,8 @@ class Trainer:
         checkpoint_callback = ModelCheckpoint(
             dirpath=self.args.ckpt_path,  # Directory to save checkpoints
             filename="model_{epoch:02d}_{val_loss:.5f}_{val_rouge:.5f}",  # Naming convention
-            monitor="val_loss",  # Metric to monitor for saving best checkpoints
-            mode="min",  # Whether to minimize or maximize the monitored metric
+            monitor="val_coco_BLEU_4",  # Metric to monitor for saving best checkpoints
+            mode="max",  # Whether to minimize or maximize the monitored metric
             save_top_k=1,  # Number of best checkpoints to keep
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
