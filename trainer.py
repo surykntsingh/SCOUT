a2150129ac7289dc0b859b38330577641a688389
Suryakant Singh <surykntsingh@gmail.com>
2025-12-22 07:13:43 +0530

    updated encoder

--- trainer.py
+++ trainer.py
@@ -41,9 +41,9 @@ class Trainer:
 
         checkpoint_callback = ModelCheckpoint(
             dirpath=self.args.ckpt_path,  # Directory to save checkpoints
-            filename="model_{epoch:02d}_{val_loss:.5f}_{val_coco_BLEU_4:.5f}",  # Naming convention
-            monitor="val_coco_BLEU_4",  # Metric to monitor for saving best checkpoints
-            mode="max",  # Whether to minimize or maximize the monitored metric
+            filename="model_{epoch:02d}_{val_loss:.5f}_{val_loss:.5f}",  # Naming convention
+            monitor="val_loss",  # Metric to monitor for saving best checkpoints
+            mode="min",  # Whether to minimize or maximize the monitored metric
             save_top_k=3,  # Number of best checkpoints to keep
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
