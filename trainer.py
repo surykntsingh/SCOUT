d0e096c4f0afb7a00f058719f712c40ae09fec78
Suryakant Singh <surykntsingh@gmail.com>
2026-04-15 18:44:48 -0400

    Updated dataset

--- trainer.py
+++ trainer.py
@@ -47,7 +47,7 @@ class Trainer:
             save_top_k=3,  # Number of best checkpoints to keep
             save_last=True  # Save the last checkpoint regardless of the monitored metric
         )
-        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-5, patience=20, verbose=True, mode="min")
+        early_stop_callback = EarlyStopping(monitor="val_coco_BLEU_1", min_delta=1e-5, patience=20, verbose=True, mode="max")
 
         # suggested_lr = self.find_lr(model, datamodule)
         # print(f'setting lr: {suggested_lr}')
